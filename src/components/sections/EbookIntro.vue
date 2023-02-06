f1202c14106fc24a531392556ebff82214c2df37
Ludovit Scholtz <ludko2@gmail.com>
2023-02-06 16:58:18 +0100

    update intro

--- src/components/sections/EbookIntro.vue
+++ src/components/sections/EbookIntro.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="ebook-feature section-space--pt_120 section-space--pb_60 section-space--mt_60" id="intro">
-        <div class="container">
+    <div class="ebook-feature " id="intro">
+        <div class="container section-space--pt_120 section-space--pb_60">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="section-title-wrapper text-center section-space--mb_60">
-                        <h2 class="section-title font-weight--blod wow move-up">We deliver the smart contracts to issue stablecoins on Algorand</h2>
+                    <div class="section-title-wrapper text-center section-space--mb_60 section-space--pt_120">
+                        <h2 class="section-title font-weight--blod wow move-up">We delivered the proof of concept to issue stablecoins on Algorand using smart contracts</h2>
                     </div>
                 </div>
             </div>
@@ -27,11 +27,13 @@
                         <p><a href="https://gitcoin.co/issue/29663" target="_blank">https://gitcoin.co/hackathon/greenhouse3/</a></p>
                         <p>During the hackathon: </p>
                         <div class="pricing-table__inner">
-                    <div class="pricing-table__body"><ul class="pricing-table__list text-left"><li>the project has been thought through, </li><li>the Whitepaper has been written, </li><li>and MVP has been deployed to the algorand testnet.</li>
-                        <li>Two open source github repos has been created:</li>
-                        <li><a href="https://github.com/scholtz/stabilitas-apps" target="_blank">https://github.com/scholtz/stabilitas-apps</a></li>
-                        <li><a href="https://github.com/scholtz/stabilitas-web" target="_blank">https://github.com/scholtz/stabilitas-web</a></li>
-                        </ul></div>
+                        <div class="pricing-table__body">
+                            <ul class="pricing-table__list text-left"><li>the project has been thought through, </li><li>the <b><a href="/white-paper-stabilitas-20230206.pdf" target="_blank">Whitepaper</a></b> has been written, </li><li>and PoC has been deployed to the algorand testnet.</li>
+                                <li>Two open source github repos has been created:</li>
+                                <li><a href="https://github.com/scholtz/stabilitas-apps" target="_blank">https://github.com/scholtz/stabilitas-apps</a></li>
+                                <li><a href="https://github.com/scholtz/stabilitas-web" target="_blank">https://github.com/scholtz/stabilitas-web</a></li>
+                            </ul>
+                        </div>
                     </div></div>
                 </div>
             </div>
