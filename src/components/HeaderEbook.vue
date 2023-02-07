75083d769094ca9fd0401e7c2536376b392492be
Ludovit Scholtz <ludko2@gmail.com>
2023-02-07 21:47:25 +0100

    link to faq

--- src/components/HeaderEbook.vue
+++ src/components/HeaderEbook.vue
@@ -27,7 +27,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#review" @click="menuCollapseOnLinkClick">
+                            <a class="nav-link" href="#faq" @click="menuCollapseOnLinkClick">
                                 <span>PoC on testnet</span>
                             </a>
                         </li>
